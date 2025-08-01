b133de44939b4013c6311488ba90796223283add
lmyers421 <myers2le@mail.uc.edu>
2025-07-31 22:39:14 -0400

    Created ASCII funciton

--- hello.py
+++ hello.py
@@ -1,33 +1,41 @@
 from PIL import Image
 
-file = open("lebron.txt", "w")
+def asciiConvert(imageName):
+    file = open("lebron.txt", "w")
 
-img = Image.open("lebron.png")
-img = img.convert("RGB")
+    img = Image.open(imageName)
+    img = img.convert("RGB")
 
-w = img.width
-h = img.height
+    w = img.width
+    h = img.height
 
-for y in range(h):
-    for x in range(w):
-        px = img.getpixel([x,y])
-        bright = px[0] + px[1] + px[2]
-        if bright == 0:
-            file.write("@")
-        elif bright < 100:
-            file.write("%")
-        elif bright < 200:
-            file.write("X")
-        elif bright < 300:
-            file.write("k")
-        elif bright < 400:
-            file.write("l")
-        elif bright < 500:
-            file.write("/")
-        elif bright < 600:
-            file.write(";")
-        elif bright < 700:
-            file.write("'")
-        else:
-            file.write("`")
-    file.write("\n")
+    for y in range(h):
+        for x in range(w):
+            px = img.getpixel([x,y])
+            bright = px[0] + px[1] + px[2]
+            if bright == 0:
+                file.write("@")
+            elif bright < 100:
+                file.write("%")
+            elif bright < 200:
+                file.write("X")
+            elif bright < 300:
+                file.write("k")
+            elif bright < 400:
+                file.write("l")
+            elif bright < 500:
+                file.write("/")
+            elif bright < 600:
+                file.write(";")
+            elif bright < 700:
+                file.write("'")
+            else:
+                file.write("`")
+        file.write("\n")
+
+if __name__ == '__main__':
+    imageName = input("Enter the name of the image file you would like to convert to ASCII: ")
+    asciiConvert(imageName)
+    print("Your ASCII art has been created!")
+
+
